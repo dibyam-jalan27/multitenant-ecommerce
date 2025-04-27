5c85469dd006613ba0adfb47d91a8ba96067d21b
Dibyam <dibyam1jalan@gmail.com>
2025-04-27 13:00:45 +0530

    custom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
     <div>
-      <p className="text-rose-500">Hello World</p>
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I am a button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am an input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+      </div>
     </div>
   );
 }
